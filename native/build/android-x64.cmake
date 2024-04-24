6521f15d7ceef265bb0541547ad6aac685a7d76b
Leandro <18240088+leandroconsiglio@users.noreply.github.com>
2024-04-24 15:19:31 -0300

    Update android-x64.cmake

--- native/build/android-x64.cmake
+++ native/build/android-x64.cmake
@@ -2,8 +2,8 @@ set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(ANDROID_ABI x86_64)
 
-set(CMAKE_C_COMPILER x86_64-linux-android24-clang)
-set(CMAKE_CXX_COMPILER x86_64-linux-android24-clang++)
+set(CMAKE_C_COMPILER x86_64-linux-android-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-android-g++)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
