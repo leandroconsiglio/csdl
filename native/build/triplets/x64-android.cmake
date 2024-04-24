2c5f59a38d9d1a7fb2f90631741bb88bece23eb2
Leandro <18240088+leandroconsiglio@users.noreply.github.com>
2024-04-24 18:19:12 -0300

    Update x64-android.cmake

--- native/build/triplets/x64-android.cmake
+++ native/build/triplets/x64-android.cmake
@@ -1,7 +1,7 @@
 #[[set(VCPKG_TARGET_ARCHITECTURE x86_64)]]
 #[[set(VCPKG_CMAKE_SYSTEM_NAME Android)]]
 set(VCPKG_TARGET_TRIPLET x64-android)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE, "$ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE, $ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake)
 set(ANDROID_ABI, x86_64)
 
 include("${CMAKE_CURRENT_LIST_DIR}/triplet-common.cmake")
