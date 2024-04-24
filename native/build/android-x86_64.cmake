63f19ce93221f08b2f0b16371e570a07a2afbe77
Leandro <18240088+leandroconsiglio@users.noreply.github.com>
2024-04-24 17:39:25 -0300

    Update android-x86_64.cmake

--- native/build/android-x86_64.cmake
+++ native/build/android-x86_64.cmake
@@ -2,11 +2,8 @@ set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(ANDROID_ABI x86_64)
 
-#[[set(CMAKE_C_COMPILER x86_64-linux-android-gcc)]]
-#[[set(CMAKE_CXX_COMPILER x86_64-linux-android-g++)]]
-
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
-include($ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake)
+include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
